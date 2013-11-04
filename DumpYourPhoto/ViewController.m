d614fdf8a4849de34df1314936570d585c98d9b1
Kamil Hismatullin <kamil_hism@mail.ru>
2013-11-04 22:39:31 +0400

    auth user (without dev token yet)

--- DumpYourPhoto/ViewController.m
+++ DumpYourPhoto/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking.h>
+#import <ObjectiveGumbo.h>
 
 @interface ViewController ()
 
@@ -23,10 +25,37 @@
 
 - (IBAction) signinTapped
 {
+
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    //manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager GET:@"https://dumpyourphoto.com" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        OGNode *data = [ObjectiveGumbo parseDocumentWithString:operation.responseString];
+        OGElement *input = [data elementsWithID:@"form_dyp_csrf_token"].firstObject;
+        NSString *csrf_token = input.attributes[@"value"];
+        
+        NSDictionary *parameters = @{
+            @"username": _usernameField.text,
+            @"password": _passwordField.text,
+            @"dyp_csrf_token": csrf_token
+        };
+        
+        [manager POST:@"https://dumpyourphoto.com/user/log_in" parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSLog(@"JSON: %@", operation.responseString);
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Error: %@", error);
+        }];
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+    
+    /*
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Can not log in. Please check username and password." delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
     [alert show];
-    
-    NSLog(@"%@", _usernameField.text);
+    */
 }
 
 @end
