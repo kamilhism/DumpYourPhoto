6fc7820c08274226c4bce2d77f5d83712cf45dd7
Kamil Hismatullin <kamil_hism@mail.ru>
2013-11-04 20:05:49 +0400

    DumpYourPhoto project init

---
+++ DumpYourPhoto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DumpYourPhoto
+//
+//  Created by Администратор on 11/4/13.
+//  Copyright (c) 2013 KamilHism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DumpYourPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DumpYourPhoto
+//
+//  Created by Администратор on 11/4/13.
+//  Copyright (c) 2013 KamilHism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
